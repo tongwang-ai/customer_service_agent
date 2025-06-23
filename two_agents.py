f9653d6c8d8c43dfe9d8ceb68c57985be310cd65
Tong Wang <wangtongada@gmail.com>
2025-06-23 16:46:01 -0400

    Update two_agents.py

--- two_agents.py
+++ two_agents.py
@@ -147,6 +147,7 @@ with col1:
         if user_message:
             st.session_state["chat_history_agent_1"].append({"role": "user", "content": user_message})
             st.session_state["await_agent_response_agent_1"] = True
+            st.session_state["user_input_agent_1"] = ""
             st.rerun()
 
     # After rerun, if a user message was just appended but no agent response yet, generate it:
